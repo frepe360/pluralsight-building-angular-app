2d1dcbdfb35b901e5c569367f42c0b7d6454cf43
Fredrik Persson <frepe360@student.liu.se>
2015-04-08 08:25:45 +0200

    Add user signup stuff

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose'),
-    crypto = require('crypto');
+    userModel = require('../models/User');
 
 module.exports = function(config) {
   mongoose.connect(config.db);
@@ -9,49 +9,6 @@ module.exports = function(config) {
     console.log('multivision db opened');
   });
 
-  var userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    username: String,
-    salt: String,
-    hashed_pwd: String,
-    roles: [String]
-  });
+  userModel.createDefaultUsers();
 
-  userSchema.methods = {
-    authenticate: function(passwordToMatch) {
-      return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
-    }
-  }
-
-  var User = mongoose.model('User', userSchema);
-
-  User.find({}).exec(function(err, collection) {
-    if(collection.length === 0) {
-      console.log('I will now populate the database');
-      var salt, hash;
-      salt = createSalt();
-      hash = hashPwd(salt, 'joe');
-      User.create({firstName: 'Joe', lastName: 'Eames', username: 'joe', salt: salt, hashed_pwd: hash, roles: ['admin']});
-      salt = createSalt();
-      hash = hashPwd(salt, 'frepe');
-      User.create({firstName: 'Fredrik', lastName: 'Persson', username: 'frepe', salt: salt, hashed_pwd: hash, roles: []});
-      salt = createSalt();
-      hash = hashPwd(salt, 'alice');
-      User.create({firstName: 'Alice', lastName: 'Persson', username: 'alice', salt: salt, hashed_pwd: hash});
-    }
-    else {
-      console.log('the database was already populated');
-    }
-  })
 }
-
-function createSalt() {
-  return crypto.randomBytes(128).toString('base64');
-}
-
-function hashPwd(salt, pwd) {
-  var hmac = crypto.createHmac('sha1', salt);
-  return hmac.update(pwd).digest('hex');
-}
-
---
+++ server/models/User.js
@@ -0,0 +1,44 @@
+var mongoose = require('mongoose'),
+    encrypt = require('../utilities/encryption');
+
+var userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    username: String,
+    salt: String,
+    hashed_pwd: String,
+    roles: [String]
+});
+
+userSchema.methods = {
+    authenticate: function(passwordToMatch) {
+        return encrypt.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+    }
+}
+
+var User = mongoose.model('User', userSchema);
+
+function createDefaultUsers() {
+    User.find({}).exec(function(err, collection) {
+        if(collection.length === 0) {
+            console.log('I will now populate the database');
+            var salt, hash;
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'joe');
+            User.create({firstName: 'Joe', lastName: 'Eames', username: 'joe', salt: salt, hashed_pwd: hash, roles: ['admin']});
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'frepe');
+            User.create({firstName: 'Fredrik', lastName: 'Persson', username: 'frepe', salt: salt, hashed_pwd: hash, roles: []});
+            salt = encrypt.createSalt();
+            hash = encrypt.hashPwd(salt, 'alice');
+            User.create({firstName: 'Alice', lastName: 'Persson', username: 'alice', salt: salt, hashed_pwd: hash});
+        }
+        else {
+            console.log('the database was already populated');
+        }
+    })
+};
+
+exports.createDefaultUsers = createDefaultUsers;
+
+
